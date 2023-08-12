c01d8d27dc03180883ba7c1d04ed8e1bff9a3682
Tommy <spielhoelle@posteo.net>
2023-08-12 14:24:07 +0100

    feat: first rede crawl

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import { PuppeteerCrawler, Dataset } from 'crawlee'
-export const site = 'www.rede.jp'
+// export const site = 'www.rede.jp'
+// export const datasetName = 'rede';
+export const site = 'www.rede.jp/'
 export const datasetName = 'rede';
 
 (async () => {
     const crawler = new PuppeteerCrawler({
-        maxRequestsPerCrawl: 10000,
+        maxRequestsPerCrawl: 100000,
 
         async requestHandler({ request, page, enqueueLinks, log }) {
             const title = await page.title()
             log.info(`Title of ${request.url}: ${title}`)
 
             // A function to be evaluated by Puppeteer within the browser context.
-            const data = await page.$$eval('body', ($posts, title) => {
+            const data = await page.$$eval('html', ($posts, title) => {
                 const scrapedData: any = []
 
                 // We're getting the title, rank and URL of each post on Hacker News.
                 $posts.forEach(($post) => {
                     scrapedData.push({
                         title: title,
-                        body: $post.innerHTML,
+                        body: $post.outerHTML,
                     })
                 })
                 return scrapedData
--- src/reduce.ts
+++ src/reduce.ts
@@ -3,53 +3,72 @@ import fs from 'fs'
 // import { datasetName } from './main'
 const datasetName = 'rede';
 (async () => {
+    let selectors = {
+        "em": "",
+        "fn": "",
+        "ln": "",
+        "ph": "",
+        "ct": "",
+        "st": "",
+        "zp": "",
+        "country": ""
+    }
     const dataset = await Dataset.open(datasetName)
     // calling reduce function and using memo to calculate number of headers
+    let allInputs: { name: string, element: any }[] = []
     const pagesHeadingCount = await dataset.reduce((memo, value) => {
         if (value.body.indexOf("<form") !== -1) {
-            let selectors = {
-                "em": "",
-                "fn": "",
-                "ln": "",
-                "ph": "",
-                "ct": "",
-                "st": "",
-                "zp": "",
-                "country": ""
-            }
             const justFormElements = [...value.body.replace(/\n/g, '').matchAll(/<form.*?<\/form>/g)]
             justFormElements.forEach(match => {
-                let allInputs = []
                 const inputs = [...match[0].matchAll(/<input.*?>/g)].map(input => input[0])
                 inputs.forEach(input => {
                     const nameField = input.match(/name="(.*?)"/)
                     const typeField = input.match(/type="(.*?)"/)
                     if (nameField && nameField.length > 1) {
                         // if (valueField && valueField.length > 1) {
-                        if (typeField[1].includes('mail')) {
-                            selectors['em'] = `${selectors['em']},${nameField[1]}`
+                        if (!selectors['em'].includes(nameField[1]) && typeField[1].includes('mail')) {
+                            selectors['em'] = `${nameField[1]},${selectors['em']}`.replace(/,$/, '')
                         }
-                        // if (!allInputs.includes(nameField[1])) {
-                        //     allInputs.push({
-                        //         name: nameField[1],
-                        //         type: typeField && typeField.length > 1 ? typeField[1] : 'text'
-                        //     })
-                        // }
-                        // }
+                        if (!selectors['ph'].includes(nameField[1]) && typeField[1].includes('tel')) {
+                            selectors['ph'] = `${nameField[1]},${selectors['ph']}`.replace(/,$/, '')
+                        }
+                        if (!selectors['zp'].includes(nameField[1]) && nameField[1].includes("postal")) {
+                            selectors['zp'] = `${nameField[1]},${selectors['zp']}`.replace(/,$/, '')
+                        }
+                        if (!selectors['fn'].includes(nameField[1]) && nameField[1].includes("first")) {
+                            selectors['fn'] = `${nameField[1]},${selectors['fn']}`.replace(/,$/, '')
+                        }
+                        if (!selectors['ln'].includes(nameField[1]) && nameField[1].includes("last")) {
+                            selectors['ln'] = `${nameField[1]},${selectors['ln']}`.replace(/,$/, '')
+                        }
+                        if (!selectors['st'].includes(nameField[1]) && nameField[1].includes("street")) {
+                            selectors['st'] = `${nameField[1]},${selectors['st']}`.replace(/,$/, '')
+                        }
+                        if (!selectors['ct'].includes(nameField[1]) && nameField[1].includes("city")) {
+                            selectors['ct'] = `${nameField[1]},${selectors['ct']}`.replace(/,$/, '')
+                        }
+                        if (!allInputs.map(i => i.name).includes(nameField[1])) {
+                            allInputs.push({
+                                name: nameField[1],
+                                // type: typeField && typeField.length > 1 ? typeField[1] : 'text'
+                                element: input
+                            })
+                        }
                     }
                 })
                 // @ts-ignore
                 memo.push({
                     title: value.title,
                     form: match[0].replace(/.*(<form.*?>).*/, "$1"),
-                    // inputs
+                    inputs,
                     selectors
                 })
             })
         }
         return memo
     }, [])
-    console.log('selectors', selectors)
+    console.log('allInputs', allInputs.map(i => i.name))
+    console.log('pagesHeadingCount', pagesHeadingCount)
     // saving result of map to default Key-value store
     // await KeyValueStore.setValue(datasetName + "_result", pagesHeadingCount)
     fs.writeFileSync(`result_${datasetName}.json`, JSON.stringify(pagesHeadingCount, null, 2))
